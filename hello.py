091c30fb20af52c9b6404ea43e50341a96750242
Vladyslav Zinets <vlad.zinets@gmail.com>
2024-08-16 21:10:16 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+number = int(input("Введіть чотирьохзначне число: "))
+num1= number // 1000
+num2= number // 100 % 10
+num3= number // 10 % 10
+num4= number % 10
+print(num1)
+print(num2)
+print(num3)
+print(num4)
