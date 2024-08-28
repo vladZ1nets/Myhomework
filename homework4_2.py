d940767b68d8ce29b412bd9a41e1b4c1838e1eb9
vladZ1nets <vlad.zinets@gmail.com>
2024-08-28 12:26:31 +0300

    Create homework4_2.py

---
+++ homework4_2.py
@@ -0,0 +1,22 @@
+while True:
+    number1 = int(input('First number: '))
+    number2 = int(input('Second number: '))
+    symbol = input('Math symbol (+, -, *, /): ')
+
+    match symbol:
+        case '+':
+            print(number1 + number2)
+        case '-':
+            print(number1 - number2)
+        case '*':
+            print(number1 * number2)
+        case '/':
+            if number2 != 0:
+                print(number1 / number2)
+            else:
+                print('The divisor cannot be equal to 0')
+        case _:
+            print('Incorrect symbol')
+    next_calc = input('if you want to continue - write \'yes\'. And if you want exit - write something else')
+    if next_calc != 'yes':
+        break
